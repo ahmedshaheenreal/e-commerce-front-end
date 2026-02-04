13215b1f58e19e9d8f798c236ec39a1cc26506b3
Ahmed shaheen <ahmedshaheenwork@gmail.com>
2026-02-04 14:33:13 +0300

    editing layouts

--- src/app/(private)/(privateTabs)/wishlist/layout.tsx
+++ src/app/(private)/(privateTabs)/wishlist/layout.tsx
@@ -1,18 +1,9 @@
-"use client";
-import { BASE_API_URL } from "@/CONSTANTS";
-
-import { WishListItemsResponse } from "@/types";
 import WishlistHydrator from "@/providers/WishlistHydrator";
 async function layout({ children }: { children: React.ReactNode }) {
   try {
-    const response = await fetch(`${BASE_API_URL}/wishlist`, {
-      credentials: "include",
-    });
-
-    const wishlist: WishListItemsResponse = await response.json();
     return (
       <>
-        <WishlistHydrator initialWishlist={wishlist || null} />
+        <WishlistHydrator initialWishlist={null} />
         {children}
       </>
     );
--- src/app/(private)/cart/layout.tsx
+++ src/app/(private)/cart/layout.tsx
@@ -1,19 +1,12 @@
-"use client";
-
 import { BASE_API_URL } from "@/CONSTANTS";
 import React from "react";
 import CartHydrator from "@/providers/CartProvider";
 import { CartItemsResponse } from "@/types";
 async function layout({ children }: { children: React.ReactNode }) {
   try {
-    const response = await fetch(`${BASE_API_URL}/cart`, {
-      credentials: "include",
-    });
-
-    const cart: CartItemsResponse = await response.json();
     return (
       <>
-        <CartHydrator initialCart={cart || null} />
+        <CartHydrator initialCart={null} />
         {children}
       </>
     );
