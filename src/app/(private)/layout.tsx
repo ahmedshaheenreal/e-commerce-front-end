4b83f61441194f4a4725234cddc3dc2fe95366dd
Ahmed shaheen <ahmedshaheenwork@gmail.com>
2026-02-04 13:27:37 +0300

    cheking redirect

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -8,7 +8,7 @@ export default async function ProtectedLayout({
   children: React.ReactNode;
 }) {
   const user = await getCurrentUser();
-
+  console.log("ProtectedLayout user:", user);
   if (!user) {
     redirect("/login");
   }
