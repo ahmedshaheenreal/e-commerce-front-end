1b006de9b035019c0fc8f0bcce37ba5926e5acab
Ahmed shaheen <ahmedshaheenwork@gmail.com>
2026-02-04 13:43:03 +0300

    making protected layout dynamic

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,6 +1,7 @@
 // app/(protected)/layout.tsx
 import { getCurrentUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
+export const dynamic = "force-dynamic";
 
 export default async function ProtectedLayout({
   children,
