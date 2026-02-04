b17f5672990c57e08aea3e3d9e93299836be331f
Ahmed shaheen <ahmedshaheenwork@gmail.com>
2026-02-04 14:13:07 +0300

    depending on client fetch on profile

--- src/app/(private)/(privateTabs)/profile/page.tsx
+++ src/app/(private)/(privateTabs)/profile/page.tsx
@@ -1,21 +1,6 @@
-import BreadCumber from "@/components/global/BreadCumber";
 import UserDataForm from "@/components/Profile/UserDataForm";
-import { BASE_API_URL } from "@/CONSTANTS";
-import { cookies } from "next/headers";
-import SideBar from "@/components/global/SideBar";
-export const dynamic = "force-dynamic";
 async function page() {
   try {
-    const cookieStore = await cookies();
-    const response = await fetch(`${BASE_API_URL}/user/profile`, {
-      headers: {
-        Cookie: cookieStore.toString(),
-      },
-    });
-    const data = await response.json();
-    if (!response.ok) {
-      throw data;
-    }
     return <UserDataForm />;
   } catch (error) {
     console.log("This is the Error");
