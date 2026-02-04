7690ef751dc779d6c355b8a537c6c38607429364
Ahmed shaheen <ahmedshaheenwork@gmail.com>
2026-02-04 13:49:54 +0300

    removing cookies header from fetch user

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,13 @@
 "use server";
 
-import { cookies } from "next/headers";
 import { BASE_API_URL } from "@/CONSTANTS";
 // lib/auth.ts
 
 export const getCurrentUser = async () => {
   try {
-    const cookieStore = await cookies();
     const response = await fetch(`${BASE_API_URL}/user/profile`, {
       credentials: "include",
-      headers: {
-        Cookie: cookieStore.toString(),
-      },
+
       cache: "no-store",
     });
 
